95f899cd0cf5b516f822ecfc7837ba1e6f940596
K2Mies <rhvidste@student.hive.fi>
2026-01-13 15:35:10 +0200

    fixed core loop logic for Server.cpp now it seems to work

--- ft_irc/src/Server.cpp
+++ ft_irc/src/Server.cpp
@@ -71,79 +71,76 @@ void Server::run(){
           perror( "poll"  );
           break ;
     }
-  }
+    // --------------------------------------------------------------- listen socket
+    if (  poll_fds[0].revents & POLLIN  ){
 
-  // --------------------------------------------------------------- listen socket
-  if (  poll_fds[0].revents & POLLIN  ){
+      int client_fd = accept( _listen_socket_fd, NULL, NULL );
 
-    int client_fd = accept( _listen_socket_fd, NULL, NULL );
+      if( client_fd >= 0) {
 
-    if( client_fd >= 0) {
+          fcntl(  client_fd, F_SETFL, O_NONBLOCK  );
+         _clients_by_fd[client_fd] = new Client(client_fd  );
+         poll_fds.push_back( pollfd{  client_fd, POLLIN, 0 } );
 
-        fcntl(  client_fd, F_SETFL, O_NONBLOCK  );
-        _clients_by_fd[client_fd] = new Client(client_fd  );
-        poll_fds.push_back( pollfd{  client_fd, POLLIN, 0 } );
-
-      std::cout
-                << "Client connected fd=" 
-                << client_fd
-                << std::endl;
+        std::cout
+                  << "Client connected fd=" 
+                  << client_fd
+                  << std::endl;
+      }
     }
-  }
-
-  // -------------------------------------------------------------- client sockets
-  for ( size_t i = 1; i < poll_fds.size(); i++) {
-        pollfd& poll_fd = poll_fds[i];
-        Client* client  = _clients_by_fd[poll_fd.fd];
-
-        // ------------------------------------------------------------------------ read
-        if  ( poll_fd.revents & POLLIN  ) {
-          char  buf[4096];
-          size_t n = recv(  poll_fd.fd, buf, sizeof(  buf ), 0);
-
-          if  ( n <= 0  ) {
-
-            std::cout
-                      << "Client disconnected fd="
-                      << poll_fd.fd
-                      << std::endl;
-
-            close(  poll_fd.fd  );
-            delete  client;
-            _clients_by_fd.erase( poll_fd.fd );
-            poll_fds.erase(poll_fds.begin() + i );
-            
-            --i;
-            continue;
+    // -------------------------------------------------------------- client sockets
+    for ( size_t i = 1; i < poll_fds.size(); i++) {
+          pollfd& poll_fd = poll_fds[i];
+          Client* client  = _clients_by_fd[poll_fd.fd];
+  
+          // ------------------------------------------------------------------------ read
+          if  ( poll_fd.revents & POLLIN  ) {
+            char  buf[4096];
+            size_t n = recv(  poll_fd.fd, buf, sizeof(  buf ), 0);
+  
+            if  ( n <= 0  ) {
+  
+              std::cout
+                        << "Client disconnected fd="
+                        << poll_fd.fd
+                        << std::endl;
+  
+              close(  poll_fd.fd  );
+              delete  client;
+              _clients_by_fd.erase( poll_fd.fd );
+              poll_fds.erase(poll_fds.begin() + i );
+              
+              --i;
+              continue;
+            }
+  
+            client->inbuf().append( buf, n );
+  
+            std::string line;
+            while ( client->popLine(  line  ) ) {
+              std::cout
+                        << "LINE: ["
+                        << line
+                        << "]"
+                        << std::endl;
+              client->queue("ECHO :" + line);
+            }
+          } 
+          // ----------------------------------------------------------------------- write
+          if (  poll_fd.revents & POLLOUT) {
+                std::string& out  = client->outbuf();
+                ssize_t sent      = send(poll_fd.fd, out.data(), out.size(), 0);
+                if (  sent > 0  ) {
+                  out.erase(  0, sent );
+                }
           }
-
-          client->inbuf().append( buf, n );
-
-          std::string line;
-          while ( client->popLine(  line  ) ) {
-            std::cout
-                      << "LINE: ["
-                      << line
-                      << "]"
-                      << std::endl;
-            client->queue("ECHO :" + line);
+  
+          // ----------------------------------------------------- update POLLOUT interest
+          if  ( client->hasPendingOutput()  ) {
+            poll_fd.events = POLLIN | POLLOUT;
+          } else  {
+            poll_fd.events = POLLIN;
           }
-        }
-
-        // ----------------------------------------------------------------------- write
-        if (  poll_fd.revents & POLLOUT) {
-              std::string& out  = client->outbuf();
-              ssize_t sent      = send(poll_fd.fd, out.data(), out.size(), 0);
-              if (  sent > 0  ) {
-                out.erase(  0, sent );
-              }
-        }
-
-        // ----------------------------------------------------- update POLLOUT interest
-        if  ( client->hasPendingOutput()  ) {
-          poll_fd.events = POLLIN | POLLOUT;
-        } else  {
-          poll_fd.events = POLLIN;
-        }
+    }
   }
 }
